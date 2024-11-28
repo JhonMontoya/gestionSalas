474e8279f3f765bd5a98a073f1c313c3f1b149e2
Jhonathan <jhonathan.guerrero.96@gmail.com>
2024-11-28 00:41:02 -0500

    prueba env

---
+++ config/db.js
@@ -0,0 +1,11 @@
+//Requerimos el modulo de mongo para la conexion a la base de datos
+const {ObjectId, MongoClient, ServerApiVersion} = require('mongodb');
+
+const conexionDB = async() =>{
+    try {
+        await mongoose.connect
+    } catch (error) {
+        
+    }
+}
+
