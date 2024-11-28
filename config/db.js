9685d4c94e88cf45d6124de03ccd25c0c05145dd
Jhonathan <jhonathan.guerrero.96@gmail.com>
2024-11-28 01:59:04 -0500

    estructura del proyecto

--- config/db.js
+++ config/db.js
@@ -1,11 +1,19 @@
 //Requerimos el modulo de mongo para la conexion a la base de datos
-const {ObjectId, MongoClient, ServerApiVersion} = require('mongodb');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const conexionDB = async() =>{
+const conexionDB = async () => {
     try {
-        await mongoose.connect
+        await mongoose.connect(process.env.MONGO_URI, {
+           useNewUrlParser: true,
+           useUnifiedTopology: true, 
+        });
+        console.log("Conexión exitosa a la base de datos");
     } catch (error) {
-        
+        console.log("Error al conectar la base de datos: ", error);
+        process.exit(1);
     }
+    
 }
 
+module.exports = conexionDB;
